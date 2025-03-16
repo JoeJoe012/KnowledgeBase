421d8aafa8fcdefb4fb6d83166acf52135f27c49
JoeJoe012 <>
2025-03-16 15:05:36 +0800

    Add base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,8 @@ import { set_sidebar } from './util/auto-gen-sidebar.mjs';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  head: [["link", { rel: "icon", href: "/github-fill.svg" }]],
+  base: 'KnowledgeBase',
+  head: [["link", { rel: "icon", href: "/KnowledgeBase/github-fill.svg" }]],
   title: "Demo Project",
   description: "A VitePress Site",
   themeConfig: {
